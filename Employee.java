6150519f2685f16ff46204bd811547985c1adf03
AKhalid000 <110781108+AKhalid000@users.noreply.github.com>
2022-08-16 11:26:46 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,12 +1,40 @@
-package org.websparrow.model;
+package com.javainuse.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	private long id;
 
-	// Getters and Setters...
 	private String name;
-	private String designation;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
 	private String dept;
-	private int salary;
-	private String location;
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", dept=" + dept + "]";
+	}
 
 }
+	
