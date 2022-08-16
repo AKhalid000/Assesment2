1d30bf3614445556cfd2812242fed7fb0da64d06
AKhalid000 <110781108+AKhalid000@users.noreply.github.com>
2022-08-16 11:09:00 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+package org.websparrow.model;
+
+public class Employee {
+
+	// Getters and Setters...
+	private String name;
+	private String designation;
+	private String dept;
+	private int salary;
+	private String location;
+
+}
